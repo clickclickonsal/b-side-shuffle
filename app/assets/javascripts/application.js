20ae6ba3765d2429f61d664b14125c391ec6d63d
ClickClickkOnSal <s.hernandez5400@gmail.com>
2015-01-09 22:05:59 -0500

    added snap-page plugin to assest pipeline

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,4 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require jQuerySlimScroll
+//= require fullPageMin
 //= require_tree .
---
+++ app/assets/javascripts/snap-page.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  $('#fullpage').fullpage({
+    anchors: ['home-page', 'news-section'],
+    menu: '#my-menu',
+    resize: false,
+    scrollOverflow: true,
+    onLeave: function(index, nextIndex, direction){
+      //after leaving section 2
+      if(index == 1 && direction =='down'){
+        $("#header").slideDown();
+      }
+      else if(index == 2 && direction == 'up'){
+        $("#header").slideUp();
+      }
+    }
+  });
+});
