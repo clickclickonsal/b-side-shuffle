303e42ea7f9db9c939c74a6d5dd5c053cac7b7ba
ClickClickkOnSal <s.hernandez5400@gmail.com>
2015-01-11 02:02:57 -0500

    pause, play, rewind & foward feature music from soundcloud working now

--- app/assets/javascripts/menuPlayerSoundColud.js
+++ app/assets/javascripts/menuPlayerSoundColud.js
@@ -1,10 +1,55 @@
 $(document).ready(function() {
 
+  var currentSong = -1;
+  var trackId = 133312661;
+  var trackTitle = "Omajimawoma";
+  var tracksArray = []
+
+  SC.initialize({
+    client_id: gon.soundcloud_key
+  });
+  var is_playing = false, sound;
+
+  SC.get("/tracks", { user_id: 34132939, limit: 10}, function(tracks){
+    for (var i = 0; i < tracks.length; i++){
+      tracksArray.push({"title": tracks[i].title, "id": tracks[i].id})
+    }
+    console.log(tracksArray);
+  });
+
+  function getNextSong(num){
+    trackId = tracksArray[num].id
+    trackTitle = tracksArray[num].title
+  }
+
+  function playSong(){
+    SC.stream("/tracks/"+trackId, function(obj){
+      obj.play();
+      sound = obj;
+      is_playing = true;
+    });
+  }
+
+  function player(){
+    if( sound ){
+      if(is_playing){
+        sound.pause();
+        is_playing = false;
+      } 
+      else {
+        sound.play();
+        is_playing = true;
+      }
+    }
+  }
+
   $("#play-button").on("click", function(event){
   	event.preventDefault();
 
   	$(this).hide();
   	$("#pause-button").show();
+    $(".song-title").html("\""+trackTitle+"\"")
+    playSong();
   });
 
   $("#pause-button").on("click", function(event){
@@ -12,5 +57,41 @@ $(document).ready(function() {
 
   	$(this).hide();
   	$("#play-button").show();
+
+    player();
   });
+
+  $("#forward-button").on("click", function(event){
+    event.preventDefault();
+
+    $("#play-button").hide();
+    $("#pause-button").show();
+    
+    currentSong++;
+    if( currentSong === tracksArray.length){
+      currentSong = 0;
+    }
+    getNextSong(currentSong);
+    $(".song-title").html("\""+trackTitle+"\"")
+    player();
+    playSong();
+  })
+
+  $("#rewind-button").on("click", function(event){
+    event.preventDefault();
+
+    $("#play-button").hide();
+    $("#pause-button").show();
+
+    currentSong--;
+    console.log(currentSong)
+    if( currentSong === -1 || currentSong === -2){
+      currentSong = tracksArray.length -1;
+    }
+
+    getNextSong(currentSong);
+    $(".song-title").html("\""+trackTitle+"\"")
+    player();
+    playSong();
+  })
 });
