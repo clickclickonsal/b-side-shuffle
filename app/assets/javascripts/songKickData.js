69a4dc8bbd5f2fac887505e430e4e78535519b7b
ClickClickkOnSal <s.hernandez5400@gmail.com>
2015-01-12 17:13:48 -0500

    appending needed data from songkick

--- app/assets/javascripts/songKickData.js
+++ app/assets/javascripts/songKickData.js
@@ -6,16 +6,16 @@ $(document).ready(function() {
 		success: function(data){
 			var result = data.resultsPage.results.event
 			for(var r = 0; r < result.length; r++){
-			console.log(result[r].venue.displayName)
+
 			console.log(result[r].uri)
 				if( r === 0 ){
-					$("#first-show ").html(result[r].start.date);
+					$("#first-show ").html("<li>"+result[r].start.date+"<br/>"+result[r].venue.displayName+"</li><li>"+result[r].location.city+"</li><li><button><a href='"+result[r].uri+"'>Tickets</a></button></li>");
 				}
 				else if( r === 1){
-					$("#second-show").html(result[r].start.date);
+					$("#second-show").html("<li>"+result[r].start.date+"<br/>"+result[r].venue.displayName+"</li><li>"+result[r].location.city+"</li><li><button><a href='"+result[r].uri+"'>Tickets</a></button></li>");
 				}
 				else if( r === 2){
-					$("#third-show").html(result[r].start.date);
+					$("#third-show").html("<li>"+result[r].start.date+"<br/>"+result[r].venue.displayName+"</li><li>"+result[r].location.city+"</li><li><button><a href='"+result[r].uri+"'>Tickets</a></button></li>");
 				}
 			}
 		}
