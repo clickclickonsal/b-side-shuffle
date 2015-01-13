faa82be83d72e6c6eecc93967ff4c345280639d9
ClickClickkOnSal <s.hernandez5400@gmail.com>
2015-01-12 23:14:42 -0500

    deleted twitter widget js file

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,7 +14,6 @@
 //= require jquery_ujs
 //= require jQuerySlimScroll
 //= require fullPageMin
-//= require twitterWidget
 //= require snapPage
 //= require menuPlayerSoundCloud
 //= require signUpForm
