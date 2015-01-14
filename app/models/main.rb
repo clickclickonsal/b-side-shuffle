6926f5b1808f6e362ce093ce619e5d0be185e212
ClickClickkOnSal <s.hernandez5400@gmail.com>
2015-01-14 03:12:01 -0500

    edited name and editing parsing in model to store image src in the variable instead

--- app/models/main.rb
+++ app/models/main.rb
@@ -3,12 +3,14 @@ require 'httparty'
 class Main < ActiveRecord::Base
 
 	def self.tumblr_data
-		response = HTTParty.get("http://api.tumblr.com/v2/blog/bsideshuffle.tumblr.com/posts/text?api_key=#{ENV['tumblr_oauth_consumer_key']}&limit=2&filter=text}")
+		response = HTTParty.get("http://api.tumblr.com/v2/blog/bsideshuffle.tumblr.com/posts/text?api_key=#{ENV['tumblr_oauth_consumer_key']}&limit=2&filter=text")
 		response['response']['posts']
 	end
 
 	def self.tumblr_blog_image
-		response = HTTParty.get("http://api.tumblr.com/v2/blog/bsideshuffle.tumblr.com/posts/text?api_key=#{ENV['tumblr_oauth_consumer_key']}&limit=2}")
-		response['response']['posts']['body']
+		image_array = []
+		response = HTTParty.get("http://api.tumblr.com/v2/blog/bsideshuffle.tumblr.com/posts/text?api_key=#{ENV['tumblr_oauth_consumer_key']}&limit=2")
+		image_array << response['response']['posts'][0]['body'][/img.*?src="(.*?)"/i,1]
+		image_array << response['response']['posts'][1]['body'][/img.*?src="(.*?)"/i,1]
 	end
 end
