f6066dd8d3e20b8afdc84f3d12dd167d8ae63438
ClickClickkOnSal <s.hernandez5400@gmail.com>
2015-01-14 02:28:13 -0500

    grabbing only recent 2 posts from tumblr now

--- app/models/main.rb
+++ app/models/main.rb
@@ -3,7 +3,9 @@ require 'httparty'
 class Main < ActiveRecord::Base
 
 	def self.tumblr_data
+		tumblr_array = []
 		response = HTTParty.get("http://api.tumblr.com/v2/blog/bsideshuffle.tumblr.com/posts/text?api_key=#{ENV['tumblr_oauth_consumer_key']}")
-		response['response']['posts']
+		tumblr_array << response['response']['posts'][0]
+		tumblr_array << response['response']['posts'][1]
 	end
 end
