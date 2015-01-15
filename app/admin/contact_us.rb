7857f8504e755ecf1f7d0f90949d96f05c7c48da
ClickClickkOnSal <s.hernandez5400@gmail.com>
2015-01-14 23:15:18 -0500

    set up controller to create contact with ajax post, configured active admin and finished completing ajax call

--- app/admin/contact_us.rb
+++ app/admin/contact_us.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register ContactUs do
-
+  permit_params :name, :email, :subject, :message
 
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
--- app/assets/javascripts/contactUsForm.js
+++ app/assets/javascripts/contactUsForm.js
@@ -7,19 +7,21 @@ $(document).ready(function() {
   	var subjectField = $("#subject-field").val();
   	var messageField = $("#message-field").val();
 
-  	$("#name-field").val('');
-  	$("#contact-email-field").val('');
-  	$("#subject-field").val('');
-  	$("#message-field").val('');
-
   	$.ajax({
   		url: "/contact_us",
   		method: "POST",
   		dataType: "json",
-  		data: { {} },
+  		data: { contact_us: { name: nameField, email: cEmailField, subject: subjectField, message: messageField } },
   		success: function(){
-  			alert("Your Information has been successfully recieved and we will get back to you soon.")
-  		}
+  			$("#name-field").val('');
+		  	$("#contact-email-field").val('');
+		  	$("#subject-field").val('');
+		  	$("#message-field").val('');
+  			alert("Your Information has been successfully recieved and we will get back to you soon.");
+  		},
+  		failure: function(data){
+  			alert("Uh oh! Something went wrong, please make sure all fields are filled out.")
+			}
   	});
   });
 });
