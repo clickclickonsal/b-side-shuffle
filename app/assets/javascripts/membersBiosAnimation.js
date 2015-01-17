297f6491bd2b34597c8565406d3cb2ce5a63f201
ClickClickkOnSal <s.hernandez5400@gmail.com>
2015-01-17 13:27:32 -0500

    band click on for more info works now

--- app/assets/javascripts/membersBiosAnimation.js
+++ app/assets/javascripts/membersBiosAnimation.js
@@ -8,5 +8,42 @@ $(document).ready(function() {
   	else{
   		$(".members-bios-container").addClass("members-bios-container-js");
   	}
+
+  	if( $(this).context.alt === "Geoff Browning"){
+  		if($(".members-bios-container").hasClass("geoff-js")){
+  			$(".members-bios-container").removeClass("geoff-js");
+	  	}
+	  	else{
+	  		$(".members-bios-container").addClass("geoff-js");
+	  	}
+  	}
+
+  	else if( $(this).context.alt === "Carter Jones" ){
+  		if($(".members-bios-container").hasClass("carter-js")){
+  			$(".members-bios-container").removeClass("carter-js");
+	  	}
+	  	else{
+	  		$(".members-bios-container").addClass("carter-js");
+	  	}
+  	}
+
+  	else if( $(this).context.alt === "Jasper" ){
+  		if($(".members-bios-container").hasClass("jasper-js")){
+  			$(".members-bios-container").removeClass("jasper-js");
+	  	}
+	  	else{
+	  		$(".members-bios-container").addClass("jasper-js");
+	  	}
+  	}
+
+  	else if( $(this).context.alt === "Nik Sus" ){
+  		if($(".members-bios-container").hasClass("nik-js")){
+  			$(".members-bios-container").removeClass("nik-js");
+	  	}
+	  	else{
+	  		$(".members-bios-container").addClass("nik-js");
+	  	}
+  	}
+
   });
 });
