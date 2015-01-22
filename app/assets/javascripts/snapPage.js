b5c12e838c8abaf4c961c1db605eda202999356d
ClickClickkOnSal <s.hernandez5400@gmail.com>
2015-01-22 15:36:33 -0500

    up and down buttons working now

--- app/assets/javascripts/snapPage.js
+++ app/assets/javascripts/snapPage.js
@@ -36,8 +36,32 @@
 $(document).ready(function(){
   var options = {
     $menu: $('header .menu'),
+    slideSpeed: 200,
     directionThreshold: 100,
-    slideSpeed: 500
+    slideSpeed: 500,
+    onActivate: function(){
+      if( $(".active").hasClass("last-page") ){
+        $("header").slideDown();
+        $("#wrapper").panelSnap('disable');
+        $("#up-button").slideDown();
+      }
+      else{
+        $("header").slideUp();
+        $("#up-button").slideUp();
+      }
+    },
+    // onSnapStart: function(){
+    //   if( $(".active").hasClass("news") ){
+    //     $('#wrapper').panelSnap('enable')
+    //   }
+    // },
+    // onSnapStart: function(){
+    //   $("header").slideUp();
+    // },
+    keyboardNavigation: {
+      enabled: true,
+      wrapAround: false,
+    }
   };
 
   $('#wrapper').panelSnap(options);
