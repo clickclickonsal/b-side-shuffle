894ea1b7469f2122fa802ea192aace67e65b6d86
ClickClickkOnSal <s.hernandez5400@gmail.com>
2015-01-23 10:59:08 -0500

    styled hamburger menu a little more

--- app/assets/javascripts/hamburgerJs.js
+++ app/assets/javascripts/hamburgerJs.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
   $(".hamburger-icon").on("click", function(event){
   	event.preventDefault();
   	console.log("hi");
+  	$(this).toggleClass("active")
   	$(".menu").toggleClass("menu-js");
   });
   $(".nav-links").on("click", function(){
